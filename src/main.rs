c827c47fe20c576738406c02077ff8b4d834e6fe
Andy Goetz <andy@andygoetz.org>
2020-03-28 17:23:00 -0700

    Added hello world to example main

--- src/main.rs
+++ src/main.rs
@@ -7,14 +7,15 @@ extern crate panic_halt; // you can put a breakpoint on `rust_begin_unwind` to c
 // extern crate panic_itm; // logs messages over ITM; requires ITM support
 // extern crate panic_semihosting; // logs messages to the host stderr; requires a debugger
 
-use cortex_m::asm;
 use cortex_m_rt::entry;
+use cortex_m_semihosting::{debug, hprintln};
 
 #[entry]
 fn main() -> ! {
-    asm::nop(); // To not have main optimize to abort in release mode, remove when you add code
+    hprintln!("Hello, world!").unwrap();
 
-    loop {
-        // your code goes here
-    }
+    // exit QEMU
+    // NOTE do not run this on hardware; it can corrupt OpenOCD state
+    debug::exit(debug::EXIT_SUCCESS);
+    loop {} // needed since main does not return
 }
