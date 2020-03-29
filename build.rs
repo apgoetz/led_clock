2b76e3bc748cf136786f5bef753002972b77b9e5
Andy Goetz <andy@andygoetz.org>
2020-03-29 13:49:13 -0700

    Added ability to target qemu or real hardware

--- build.rs
+++ build.rs
@@ -6,13 +6,38 @@ use std::path::PathBuf;
 fn main() {
     // Put the linker script somewhere the linker can find it
     let out = &PathBuf::from(env::var_os("OUT_DIR").unwrap());
-    File::create(out.join("memory.x"))
-        .unwrap()
-        .write_all(include_bytes!("memory.x"))
-        .unwrap();
-    println!("cargo:rustc-link-search={}", out.display());
+    let mut feature_count = 0;
 
+    if cfg!(feature = "hw") {
+        feature_count += 1;
+    }
+
+    if cfg!(feature = "qemu") {
+        feature_count += 1;
+    }
+
+    if feature_count != 1 {
+        panic!("\n\nMust select exactly one package for linker script generation!\nChoices: 'qemu' or 'hw' \n\n");
+    }
+
+    if !cfg!(feature = "disable-linker-script") {
+        let linker = if cfg!(feature = "hw") {
+            include_bytes!("memory_hw.x").as_ref()
+        } else if cfg!(feature = "qemu") {
+            include_bytes!("memory_qemu.x").as_ref()
+        } else {
+            unreachable!();
+        };
+    
+    
+        File::create(out.join("memory.x"))
+            .unwrap()
+            .write_all(linker)
+            .unwrap();
+        println!("cargo:rustc-link-search={}", out.display());
+    }
     // Only re-run the build script when memory.x is changed,
     // instead of when any part of the source code changes.
-    println!("cargo:rerun-if-changed=memory.x");
+    println!("cargo:rerun-if-changed=memory_qemu.x");
+    println!("cargo:rerun-if-changed=memory_hw.x");
 }
--- src/main.rs
+++ src/main.rs
@@ -7,15 +7,26 @@ extern crate panic_halt; // you can put a breakpoint on `rust_begin_unwind` to c
 // extern crate panic_itm; // logs messages over ITM; requires ITM support
 // extern crate panic_semihosting; // logs messages to the host stderr; requires a debugger
 
-use cortex_m_rt::entry;
+use cortex_m_rt::{entry};
+
+#[cfg(feature="qemu")]
 use cortex_m_semihosting::{debug};
+#[cfg(not(feature="qemu"))]
+use stm32l0xx_hal; // needed if we are compiling on the device
+
+// helper function to abstract to quit runner. Only does things if qemu is enabled . Otherwise loops
+fn exit() -> ! {
+    #[cfg(feature="qemu")]
+    debug::exit(debug::EXIT_SUCCESS);
+    loop {}
+}
+
 
 #[entry]
 fn main() -> ! {
-    let DATA_addr = 0x4000c000;
-    let FR_addr = 0x4000c018;
-    let FR = FR_addr as *const u8;
-    let DATA =  DATA_addr as *mut u8;
+
+    let fr_reg = 0x4000c018 as *const u8;
+    let data_reg =  0x4000c000 as *mut u8;
 
     unsafe {
         
@@ -23,17 +34,15 @@ fn main() -> ! {
     // NOTE do not run this on hardware; it can corrupt OpenOCD state
         
         loop {
-            if (*FR & 0x10)  == 0 {
-                while (*FR & 0x20) != 0 {}
-                let char = *DATA;
+            if (*fr_reg & 0x10)  == 0 {
+                while (*fr_reg & 0x20) != 0 {}
+                let char = *data_reg;
                 if char == 0x03 {
-                    debug::exit(debug::EXIT_SUCCESS);
+                    exit();
                 } else {
-                    *DATA = char;
+                    *data_reg = char;
                 }
             }
         } 
-        
-        loop {} // needed since main does not return
     }
 }
