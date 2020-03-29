fc814cb68d2f469adc1bf817ada078bb82b0da15
Andy Goetz <andy@andygoetz.org>
2020-03-28 21:07:30 -0700

    Added very basic echo support using virtual serial port

--- src/main.rs
+++ src/main.rs
@@ -8,14 +8,32 @@ extern crate panic_halt; // you can put a breakpoint on `rust_begin_unwind` to c
 // extern crate panic_semihosting; // logs messages to the host stderr; requires a debugger
 
 use cortex_m_rt::entry;
-use cortex_m_semihosting::{debug, hprintln};
+use cortex_m_semihosting::{debug};
 
 #[entry]
 fn main() -> ! {
-    hprintln!("Hello, world!").unwrap();
+    let DATA_addr = 0x4000c000;
+    let FR_addr = 0x4000c018;
+    let FR = FR_addr as *const u8;
+    let DATA =  DATA_addr as *mut u8;
 
+    unsafe {
+        
     // exit QEMU
     // NOTE do not run this on hardware; it can corrupt OpenOCD state
-    debug::exit(debug::EXIT_SUCCESS);
-    loop {} // needed since main does not return
+        
+        loop {
+            if (*FR & 0x10)  == 0 {
+                while (*FR & 0x20) != 0 {}
+                let char = *DATA;
+                if char == 0x03 {
+                    debug::exit(debug::EXIT_SUCCESS);
+                } else {
+                    *DATA = char;
+                }
+            }
+        } 
+        
+        loop {} // needed since main does not return
+    }
 }
